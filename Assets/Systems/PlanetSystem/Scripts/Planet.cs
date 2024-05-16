9594604d6332587d045b6701a869064336b32a50
kamzhanyue <kamzhanyue@gmail.com>
2024-05-16 23:12:59 +1000

    Add Sprite Renderer

--- Assets/Systems/PlanetSystem/Scripts/Planet.cs
+++ Assets/Systems/PlanetSystem/Scripts/Planet.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class Planet : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private SpriteRenderer _spriteRenderer;
 
-    // Update is called once per frame
-    void Update()
+    public SpriteRenderer SpriteRenderer => _spriteRenderer;
+
+    private void Awake()
     {
-        
+        _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 }
